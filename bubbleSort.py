a0685bc0b1d6f27918bf6e8a0e04c3b62a928ca8
Venom23 <venomafamilia@gmail.com>
2018-09-22 17:56:06 +0100

    Initial commit

---
+++ bubbleSort.py
@@ -0,0 +1,10 @@
+number = [10,3,5,1,6,7,8,4,2,9]
+for loopAgain in range(len(number)-1):
+    for counter in range(len(number)-1):
+        if number[counter+1] < number[counter]:
+            temp = number[counter]
+            number[counter] = number[counter+1]
+            number[counter+1] = temp
+        print(number)
+        print(counter)
+        print(passNum)
